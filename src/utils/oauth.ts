db5bba4e7da395bc8bf7406e2615ed05239d6111
Enoch Mok <enochmokny@gmail.com>
2026-04-10 11:07:35 +0800

    add debug in dynamic cards

--- src/utils/oauth.ts
+++ src/utils/oauth.ts
@@ -64,5 +64,6 @@ export async function getValidAccessToken(
 
   // Token expired or not cached, refresh it
   const newTokens = await refreshToken(provider, oldRefreshToken);
+  console.log(newTokens);
   return newTokens?.access_token || null;
 }
