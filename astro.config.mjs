fb716003714c9f8e031d28eabcd47db7f75808aa
Enoch Mok <enochmokny@gmail.com>
2026-04-10 11:36:03 +0800

    checking to see if it will load env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import icon from "astro-icon";
 import expressiveCode from "astro-expressive-code";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -34,7 +33,6 @@ export default defineConfig({
     mdx(),
     icon(),
     sitemap(),
-    react(),
   ],
 
   env: {
