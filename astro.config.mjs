766d3f6b4e556ed47ba4afba4a301f7b086fcead
Enoch Mok <enochmokny@gmail.com>
2026-04-10 11:02:26 +0800

    server config doesnt work

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   // IMPORTANT: Change this to your deployed site URL
   site: "https://enochmok.vercel.app/",
-  output: "server",
+  output: "static",
 
   adapter: vercel({
     webAnalytics: {
