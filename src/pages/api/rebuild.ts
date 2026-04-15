6cff954d2b269c9da6ae5817e0ec6367959ac818
Enoch Mok <enochmokny@gmail.com>
2026-04-15 15:07:30 +0800

    shifted rebuild from vercel to github actions

--- src/pages/api/rebuild.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  const res = await fetch(process.env.VERCEL_DEPLOY_HOOK!, {
-    method: "POST",
-  });
-
-  return new Response("Rebuild triggered", { status: 200 });
-}
